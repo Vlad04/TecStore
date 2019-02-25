8dcabfd5ba686894a489acbdd750735335251d74
Vlad04 <vladiir.rod96@gmail.com>
2019-02-24 18:37:38 -0600

    Sprint 1

--- app/src/main/java/itesm/com/tecstore/Feed.kt
+++ app/src/main/java/itesm/com/tecstore/Feed.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 
 class Feed : AppCompatActivity() {
 
-    private val RIPPLE_DURATION: Long = 250
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
