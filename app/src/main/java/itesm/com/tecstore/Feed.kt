5e6e480f62fd788a41ce296feb526379f840b4d7
Vlad04 <vladiir.rod96@gmail.com>
2019-03-16 15:51:36 -0600

    Menu - Done

--- app/src/main/java/itesm/com/tecstore/Feed.kt
+++ app/src/main/java/itesm/com/tecstore/Feed.kt
@@ -1,7 +1,10 @@
 package itesm.com.tecstore
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import itesm.com.tecstore.R.id.configurationButton_feed
 
 class Feed : AppCompatActivity() {
 
@@ -9,5 +12,11 @@ class Feed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
+
+
+        val configurationButton_feed = findViewById<ImageView>(R.id.configurationButton_feed) as ImageView
+        // set on-click listener
+        configurationButton_feed!!.setOnClickListener { startActivity(Intent(this@Feed, Menu::class.java)) }
+
     }
 }
