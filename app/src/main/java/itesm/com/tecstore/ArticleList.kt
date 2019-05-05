32a33493f786d5a81d9d0dd5cfe7ff2ad51fead5
Vlad04 <vladiir.rod96@gmail.com>
2019-05-04 22:07:16 -0500

    sistema-pago

--- app/src/main/java/itesm/com/tecstore/ArticleList.kt
+++ app/src/main/java/itesm/com/tecstore/ArticleList.kt
@@ -1,18 +1,36 @@
 package itesm.com.tecstore
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
+import com.anjlab.android.iab.v3.BillingProcessor
+import com.anjlab.android.iab.v3.TransactionDetails
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_article_list.*
 
 
+class ArticleList : AppCompatActivity(), BillingProcessor.IBillingHandler {
+    internal var bp: BillingProcessor? = null
+    lateinit var textView: TextView
 
+    override fun onBillingInitialized() {
+    }
 
-class ArticleList : AppCompatActivity() {
+    override fun onPurchaseHistoryRestored() {
+    }
+
+    override fun onProductPurchased(productId: String, details: TransactionDetails?) {
+    }
+
+    override fun onBillingError(errorCode: Int, error: Throwable?) {
+    }
+
     lateinit var simpleList: ListView
     var ref = FirebaseDatabase.getInstance().reference
     var currentuser = FirebaseAuth.getInstance().currentUser!!.uid
@@ -67,6 +85,19 @@ class ArticleList : AppCompatActivity() {
         retrieveStuffFromDatabase()
         setContentView(R.layout.activity_article_list)
         simpleList = findViewById<ListView>(R.id.userlist) as ListView
+        bp = BillingProcessor(this, null, this)
+
+        button2.setOnClickListener {
+            bp!!.purchase(this@ArticleList, "android.test.refunded")
+            println("BILLING PROCESS= "+bp)
+        }
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (!bp!!.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
 }
